ffd3c26435fddee76783b726341bca16e5ae5a9d
NishchalRautela7 <biswasritaban@gmail.com>
2026-01-18 07:49:27 +0530

    tcl script to find factorial of a number

---
+++ factorial_using_recursion.tcl
@@ -0,0 +1,11 @@
+#to find the factorial of a umber using rescursion
+proc factorial {n} {
+    if {$n==1} {
+        return 1
+    } elseif {$n==0} {
+        puts "number should be greater than 0"
+    } else {
+        return [expr {$n * [factorial [expr {$n -1}]]}]
+        }
+}
+puts "\nFactorial of 5 is --> [factorial 5]\n"
